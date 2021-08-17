f1d342e1df1cd3517f4fe624ab73519e0ba3bdae
Srdan Cvijetic <scwebdevsc@gmail.com>
2021-08-17 12:11:46 +0200

    Update for Portfolio project 2

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close');
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
+let videoBtn = document.querySelectorAll('.vid-btn');
 
 window.onscroll = () => {
   searchBtn.classList.remove('fa-times');
@@ -13,21 +14,29 @@ window.onscroll = () => {
   navbar.classList.remove('active');
   loginForm.classList.remove('active');
 }
-
+/* ########## MENU ########## */
 menu.addEventListener('click', () => {
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 });
-
+/* ########## SEARCH ########## */
 searchBtn.addEventListener('click', () => {
   searchBtn.classList.toggle('fa-times');
   searchBar.classList.toggle('active');
 });
-
+/* ########## LOGIN ########## */
 formBtn.addEventListener('click', () => {
   loginForm.classList.add('active');
 });
-
 formClose.addEventListener('click', () => {
   loginForm.classList.remove('active');
+});
+/* ########## VIDEO ########## */
+videoBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    document.querySelector('.controls .active').classList.remove('active');
+    btn.classList.add('active');
+    let src = btn.getAttribute('data-src');
+    document.querySelector('#video-slider').src = src;
+  });
 });
